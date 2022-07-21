c70cb5e6537818560190f4d939371eb2cb21abd6
maen <maenkh@yahoo.com>
2022-07-21 18:02:47 +0300

    fix linter

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import App from './App';
+/* eslint-disable import/extensions */
 import store from './Redux/configureStore';
 
 test('renders learn react link', () => {
