78bb8fe92bc9b08873820f0a1c3dff419b1809d9
jheart-vic <adebowalevictorjheart.com>
2022-07-21 15:17:45 +0100

    Add test for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './Redux/configureStore';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
   const linkElement = screen.getByText(/Space Travel Hub/i);
   expect(linkElement).toBeInTheDocument();
 });
