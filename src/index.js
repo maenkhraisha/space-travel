7a8a8220d81fbb004afb378cc827ecad1c364174
jheart-vic <adebowalevictorjheart.com>
2022-07-21 09:42:58 +0100

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+/* eslint-disable import/extensions */
 import store from './Redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+/* eslint-disable import/extensions */
 import missionSlice from './mission/mission';
 import rocketsReducer from './Rocket/rokect-reducer';
 
