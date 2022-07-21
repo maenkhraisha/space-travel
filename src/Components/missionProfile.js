47f5fe69e6781da5ffe7ce8522f3e2fae18bc7bf
maen <maenkh@yahoo.com>
2022-07-21 17:59:07 +0300

    fix linter

--- src/Components/missionProfile.js
+++ src/Components/missionProfile.js
@@ -7,15 +7,15 @@ const MissionProfile = () => {
   return (
     <div className="reserve-container-top">
       <h3>My Missions</h3>
+      {missionsFiltered.length === 0 && <p>No Reserved Missions</p>}
       <table className="table table-bordered">
-        { missionsFiltered.length === 0 && <p>No Reserved Missions</p>}
-        {missionsFiltered.map((mission) => (
-          <tr key={mission.mission_id}>
-            <th>
-              <p>{mission.mission_name}</p>
-            </th>
-          </tr>
-        ))}
+        <thead>
+          {missionsFiltered.map((mission) => (
+            <tr key={mission.mission_id}>
+              <th>{mission.mission_name}</th>
+            </tr>
+          ))}
+        </thead>
       </table>
     </div>
   );
